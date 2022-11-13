8b447d488f2efebed60e3f0eb170a1ee321d304e
jxlxx <jul14+git@proton.me>
2022-11-13 12:16:16 -0500

    go

---
+++ go/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A flake to develop and build a go project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      {
+         devShell = pkgs.mkShell {
+           buildInputs = [
+              pkgs.go
+              pkgs.gotools
+              pkgs.golangci-lint
+              pkgs.gopls
+              pkgs.go-outline
+              pkgs.gopkgs
+           ];
+         };
+      });
+}
